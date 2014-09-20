8d0c720931a23434da0cc2caada252dcf45acec8
andrew <afriday11@gmail.com>
2014-09-20 10:50:23 +0800

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Andrew Friday on 9/20/14.
+//  Copyright (c) 2014 Andrew Friday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Andrew Friday on 9/20/14.
+//  Copyright (c) 2014 Andrew Friday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
