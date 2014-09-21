fe8951286a5b1768364a7ca268dbabc0043adc7f
andrew <afriday11@gmail.com>
2014-09-21 20:53:12 +0800

    second commit

--- UITableViewController Challenge Solution/CCTableViewController.h
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -1,13 +1,13 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
-//  Created by Andrew Friday on 9/20/14.
+//  Created by Andrew Friday on 9/21/14.
 //  Copyright (c) 2014 Andrew Friday. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
